ae095529ddeada2560991090d7e73ea878ccef74
Luan H. Medeiros <139378326+luanleprechaun@users.noreply.github.com>
2025-04-30 00:14:42 -0300

    Delete SalvarRelatorio.java

--- SalvarRelatorio.java
+++
@@ -1,9 +0,0 @@
-public class SalvarRelatorio extends GerarRelatorio implements Salvar{
-
-    @Override
-    public boolean salvarNoArquivo(String dado, String nome_arquivo) {
-        // codigo para salvar no arquivo.
-        return false;
-    }
-    
-}
