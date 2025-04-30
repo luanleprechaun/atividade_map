8c9514ac145a5de6c8b1911b93584eec4eb79b16
Luan H. Medeiros <139378326+luanleprechaun@users.noreply.github.com>
2025-04-30 00:17:32 -0300

    Add files via upload

---
+++ questao1/EnviarRelatorio.java
@@ -0,0 +1,9 @@
+public class EnviarRelatorio extends GerarRelatorio implements Enviar{
+
+    @Override
+    public boolean enviarPorEmail(String dado, String email) {
+        // codigo para enviar por email.
+        return false;
+    }
+    
+}
